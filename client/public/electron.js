d45f994830eb3c34556302304115ea3b26e49e58
Jakob Frank <jakobdfrank@gmail.com>
2024-03-16 14:15:46 -0700

    [10000] Fix debug flag

--- client/public/electron.js
+++ client/public/electron.js
@@ -2,15 +2,6 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 function createWindow() {
-  let isDev;
-  import("electron-is-dev")
-    .then((module) => {
-      isDev = module.default;
-    })
-    .catch((error) => {
-      console.error("Failed to load electron-is-dev", error);
-    });
-
   // Create the browser window.
   const win = new BrowserWindow({
     width: 1024,
@@ -22,7 +13,7 @@ function createWindow() {
 
   let pth;
 
-  if (isDev) {
+  if (!app.isPackaged) {
     pth = "http://localhost:3000";
   } else {
     pth = `file://${path.join(__dirname, "../build/index.html")}`;
